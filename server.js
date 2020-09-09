7543d64e2afabb46e69b35118531b44ac5a61cc1
Opilane TTHK <opilane@tthk.ee>
2020-09-09 10:53:38 +0300

    Suppervillains

--- server.js
+++ server.js
@@ -4,5 +4,24 @@ superheroes.all.forEach(hero =>{
     console.log(hero);
 });
 
+let supervillains = require('supervillains');
+console.log(supervillains.random());
+
+supervillains.all.forEach(villain =>{
+    console.log(villain);
+});
+
+
+console.log(`${superheroes.random()} fights ${supervillains.random()}`);
+
+
 
 //console.log(superheroes.all);
+
+/*task 
+1. get the name of the supervillains
+2. Get a random supervillains name
+3. Make a random superhero fight  a random supervillain in the concole.log()
+
+*/
+
